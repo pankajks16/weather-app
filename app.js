46cee924e79fafa7d51cffc690a2b6c3fd7502cb
Pankaj K S <SPankaj@ESTUATE.COM>
2018-01-05 18:15:18 +0530

    code commited upto URL encoding

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs.options({
+	address: {
+		demand: true,
+		alias: 'a',
+		describe: 'Enter the address of the required location',
+		string: true
+	}
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+//console.log(argv.a);
+const urlAddress = encodeURIComponent(argv.a);
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=estuate%20software%20pvt%20ltd%20yelahanka%20new%20town%20bengaluru',
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${urlAddress}}`,
 	json: true
 }, ( error, response, body) => {
 	//console.log(response);
-	console.log((body.results)[0].formatted_address);
+	try{
+	// console.log(JSON.stringify(body, undefined, 2));
+	// console.log(response.statusCode);
+	// console.log(body.status);
+
+	// console.log(error);
+	// console.log(body);
+	console.log("STATUS:" + body.status);
+	console.log(` Latitude: ${body.results[0].geometry.location.lat}`);
+	console.log(` Longitude: ${body.results[0].geometry.location.lng}`);
+	console.log(`Address: ${body.results[0].formatted_address}`); 
+	} catch(e) {
+		console.log("Unable to fetch the data from the URL");
+	}
 });
