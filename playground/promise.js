bcbc77d8eb2f0f0a48c97ec84a7a213ba86e734e
Pankaj K S <SPankaj@ESTUATE.COM>
2018-01-16 18:27:59 +0530

    promise.js updated

--- playground/promise.js
+++ playground/promise.js
@@ -1,3 +1,57 @@
-var somePromise = new Promise(( resolve, reject) => {
-	
+const yargs = require('yargs');
+
+var argv = yargs.options({ 
+	num1: {
+		demand: true,
+		alias: 'a',
+		describe: 'Enter the first number'
+	},
+	num2: {
+		demand: true,
+		alias: 'b',
+		describe: 'Enter the second number'
+	}
+})
+.help()
+.argv;
+
+
+// console.log(argv.a+argv.b);
+// console.log(argv.a,argv.b);
+
+
+var asyncAdd = (a,b) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (typeof a === 'number' && typeof b === 'number') {
+				resolve(a + b);
+			} else {
+				reject('Enter the proper number format ...');
+			}
+		}, 2000);
+	});
+};
+
+
+asyncAdd(argv.a,argv.b).then((success) => {
+	console.log('Sucess addition is: ', success);
+}, (fail) => {
+	console.log('Error', fail);
 });
+
+
+
+
+
+
+
+// var somePromise = new Promise((resolve, reject) => {
+// 	resolve('Hi this is success handler running ....');
+// 	// reject('Hi this is error handler running ....');
+// });
+
+// somePromise.then((message) => {
+// 	console.log('Success: ' + message);
+// }, (error) => {
+// 	console.log('Error: ' + error);
+// });
